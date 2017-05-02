75c5a7b654505d4cb8b75dad67d9769b4547fa7f
Cy Kong <cykong@Regie-Kongs-MacBook-Pro.local>
2017-05-01 19:51:51 -0700

    Add React components - header; working on Compare Face Component

--- App.jsx
+++ App.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './Header.jsx';
 
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      init:null
     }
   }
+
+
 
   render () {
     return (
       <div>
-        <h1> Hello World from React</h1>
+        <Header />
+        <div className="face-compare container">
+          <h2>H2</h2>
+          <p>Instructions</p>
+          <div className="compare-container">
+            <div className="first compare">
+              <div className="compare-box">
+                <div className="img-box img-box-one">
+                </div>
+                <div className="search-box">
+                </div>
+              </div>
+            </div>
+            <div className="compare">
+              <div className="compare-box">
+                <div className="img-box">
+                </div>
+                <div className="search-box">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
     )
